2f10c1c3e051c2f7bec1fdbe394b3873c6589f9a
Martín Cordero <49368120+miRepositorioGit@users.noreply.github.com>
2019-06-24 00:01:08 -0500

    Add files via upload

---
+++ FilesIno/BlinkFiles/funcionesAux.h
@@ -0,0 +1,15 @@
+/*
+ * funcionesAux.h - Creación de repertorio de funciones
+ * Created by Martín Cordero mi.cuenta.correo.en@gmail.com
+ * Revision #1 - See readMe.txt 
+ */
+
+#ifndef funcionesAux_h
+
+#include "Arduino.h"
+ 
+      void blinkLed();          // enciende-apaga pin
+      void configRecursos();    // configura recursos periféricos
+   
+ // The end wrapping of the #ifndef Include Guard
+#endif
